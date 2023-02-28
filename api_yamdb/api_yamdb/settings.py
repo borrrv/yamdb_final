39fd9f5d4fb6dbb8eaf1b5f76e33d5df0d116aa4
Владислав Бородай <vrboroday@yandex.ru>
2023-02-28 19:56:19 +0300

    settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = [os.getenv('SERVERNAME'), 'web:8000']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
